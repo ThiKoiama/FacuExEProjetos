87ae8dcf1b303d9e6fe9a47569d22267b83a6b1e
Thiago Koiama <thiagokoiama@gmail.com>
2024-05-09 19:55:08 -0300

    DSFW3

--- typescript-dsfwIII-facu/projeto-materias/src/app/services/materias.service.ts
+++ typescript-dsfwIII-facu/projeto-materias/src/app/services/materias.service.ts
@@ -14,18 +14,18 @@ export class MateriasService {
   constructor(private dataBaseStore: AngularFirestore) { }
 
   getAll() {
-    return this.dataBaseStore.collection('materias', materia => materia.orderBy('name')).valueChanges({idFiled: 'firebaseId'}) as Observable<any[]>;
+    return this.dataBaseStore.collection('aulas', materia => materia.orderBy('name')).valueChanges({idFiled: 'firebaseId'}) as Observable<any[]>;
   }
 
   create(materia: Materia) {
-    return this.dataBaseStore.collection('materias').add(materia);
+    return this.dataBaseStore.collection('aulas').add(materia);
   }
 
   update(materiaId: string, materia: Materia) {
-    return this.dataBaseStore.collection('materias').doc(materiaId).update(materia);
+    return this.dataBaseStore.collection('aulas').doc(materiaId).update(materia);
   }
 
   delete(materiaId: string) {
-    return this.dataBaseStore.collection('materias').doc(materiaId).delete();
+    return this.dataBaseStore.collection('aulas').doc(materiaId).delete();
   }
 }
